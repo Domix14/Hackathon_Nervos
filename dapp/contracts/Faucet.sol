2eb74957d691e6e054ff12898abe97e995755197
Dominik Jaworski <domix.kontakt@gmail.com>
2021-08-08 17:31:56 +0200

    Fix smart contract

--- dapp/contracts/Faucet.sol
+++ dapp/contracts/Faucet.sol
@@ -1,20 +1,20 @@
 pragma solidity >=0.8.0;
 
 contract Faucet {
-  map(address => uint256) previousReceivers;
+  mapping(address => uint256) previousReceivers;
   uint256 delay = 60 minutes;
   uint256 amount;
 
-  function receive(address receiver) public payable {
+  function receive(address payable receiver) public payable {
     require(allowedToReceive(receiver));
     receiver.transfer(amount);
-    previousReceiver[receiver] = block.timestamp + delay;
+    previousReceivers[receiver] = block.timestamp + delay;
   }
 
   function allowedToReceive(address receiver) public view returns (bool) {
-      if(previousReceiver[receiver] == 0) {
+      if(previousReceivers[receiver] == 0) {
           return true;
-      } else if(block.timestamp >= previousReceiver[receiver]) {
+      } else if(block.timestamp >= previousReceivesr[receiver]) {
           return true;
       }
       return false;
