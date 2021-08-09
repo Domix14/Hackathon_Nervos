89df7fa3365751725487c02ba04fbde23f056f07
Dominik Jaworski <domix.kontakt@gmail.com>
2021-08-09 20:20:38 +0200

    Update ERC20.sol

--- ShitcoinDeployer/contracts/ERC20.sol
+++ ShitcoinDeployer/contracts/ERC20.sol
@@ -4,27 +4,26 @@ contract BasicToken {
 
   string public constant name;
   string public constant symbol;
-  uint8 public constant decimals = 18; 
+  uint8 public constant decimals = 18;
+  uint256 totalSupply;
 
   uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
   constructor(string memory _name, string memory _symbol, uint256 _supply) public {
     name = _name;
     symbol = _symbol;
-    totalSupply_ = _supply;
+    totalSupply = _supply;
     balances[msg.sender] = _supply;
-    Transfer(0x0, msg.sender, _supply);
   }
 
   mapping(address => uint256) balances; // a mapping of all user's balances
 
   function transfer(address recipient, uint256 value) public {
-
     balances[msg.sender] -= value;
     balances[recipient] += value;
   }
 
-  function balanceOf(address account) public constant returns (uint256) {
+  function balanceOf(address account) public returns (uint256) {
     return balances[account];
   }
 
